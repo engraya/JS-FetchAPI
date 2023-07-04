5dd30dc8f0eae37dfaa57bcbed52353a968532e8
Engraya <ahmadyakubuaya@gmail.com>
2023-07-04 14:34:55 +0100

    Final Commit

--- post_Form_Data_FetchAPI.js
+++ post_Form_Data_FetchAPI.js
@@ -1,9 +1,12 @@
-const formElement = document.getElementById("myForm");
+const formElement1 = document.getElementById("myForm");
+const formElement2 = document.getElementById("myForm2");
 
-formElement.addEventListener("submit", event => {
+
+// Using Normal JSON as Data Content Type
+formElement1.addEventListener("submit", event => {
     event.preventDefault();
 
-    formData = new FormData(formElement);
+    formData = new FormData(formElement1);
 
     const dataObject = Object.fromEntries(formData);
 
@@ -19,4 +22,26 @@ formElement.addEventListener("submit", event => {
       .then(data => console.log(data))
       .catch(error => console.log(error))
  
+})
+
+
+// Using Normal x-www-form-urlEncoded as Data Content Type
+formElement2.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const formData = new FormData(formElement2);
+
+    const newURLdata = new URLSearchParams(formData);
+
+    console.log(dataObject);
+
+    fetch("https://reqres.in/api/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        body: newURLdata
+    }).then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
 })
